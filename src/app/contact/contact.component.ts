3b67ecba217aaad622305cbbacb5c894617d882a
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-07-02 15:51:29 +0530

    contact form added,  resume changed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  email = new FormControl("",[
+    Validators.required,
+    Validators.email,
+  ])
+
+  name = new FormControl("",[
+    Validators.required,
+  ])
+
+  message = new FormControl("",[
+    Validators.required,
+    Validators.minLength(15)
+  ])
+
+  ContactForm = new FormGroup({
+    email:this.email,
+    name:this.name,
+    message:this.message,
+  })
+  reset(){
+    this.ContactForm.reset();
+  }
+  send(){
+    console.log(this.ContactForm.value);
+    alert("Your message has been sent succesfully!!");
+    this.reset();
+  }
 }
