8e08fe48f7d8bddd62d0d98895ec9ea5f63b5a78
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-07-09 00:22:03 +0530

    env added to initializeApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from '../firebase';
+import { environment } from '../environments/environment.template';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,7 +14,7 @@ export class AppComponent {
   title = 'My_Portfolio';
 
   constructor() {
-    initializeApp(firebaseConfig);
+    initializeApp(environment);
   }
 
 }
