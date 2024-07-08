4905e6d2fdc6799a41d83713cda31c8f0659ee13
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-07-09 00:11:46 +0530

    env file added

---
+++ generate-env.ts
@@ -0,0 +1,15 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+// Read the template file
+const templatePath = path.join(__dirname, 'src', 'environments', 'environment.template.ts');
+const template = fs.readFileSync(templatePath, 'utf8');
+
+// Replace placeholders with environment variables
+const environmentFile = template.replace(/\${(\w+)}/g, (_, key) => process.env[key] || '');
+
+// Write the new environment file
+const outputPath = path.join(__dirname, 'src', 'environments', 'environment.ts');
+fs.writeFileSync(outputPath, environmentFile);
+
+console.log('Environment file generated successfully.');
